6e6c76116c191eb53d474925c36a1db26625c251
Juhee LEE <juheelee@Juheeui-MacBookAir.local>
2025-12-12 18:54:35 -0500

    final version

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,35 @@
-const headers = document.querySelectorAll(".accordion-header");
+  const headers = document.querySelectorAll(".accordion-header");
 
-headers.forEach(header => {
-  header.addEventListener("click", () => {
-    const content = header.nextElementSibling;
-    const arrow = header.querySelector(".arrow");
+  headers.forEach((header) => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling;
+      const arrow = header.querySelector(".arrow");
 
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-      content.classList.remove("open");
-      arrow.style.transform = "rotate(0deg)";
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-      content.classList.add("open");
-      arrow.style.transform = "rotate(180deg)";
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        content.classList.remove("open");
+        arrow.style.transform = "rotate(0deg)";
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.classList.add("open");
+        arrow.style.transform = "rotate(180deg)";
+      }
+    });
+  });
+  
+document.querySelectorAll("#cafeteria-menu a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const button = document.querySelector(
+      `.accordion-header.${link.dataset.target}`
+    );
+
+    const content = button?.nextElementSibling;
+
+    if (button && content && !content.style.maxHeight) {
+      button.click();
     }
   });
 });
+
